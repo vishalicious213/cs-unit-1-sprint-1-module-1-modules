c4697fbd9002d972aa18b57172a85aa8b300aaa5
Vish Singh <vishalicious213@gmail.com>
2020-10-05 23:23:45 +0000

    printing all functions available in "math". need to filter down to those beginning with "is"

--- main.py
+++ main.py
@@ -2,3 +2,6 @@
 Import the "math" module. Then, print an alphabetically sorted list of all the functions available in the "math" module that start with the letters "is".
 """
 # YOUR CODE HERE
+import math
+
+print(dir(math))
